de8ec42fb05952c5a84684034b4a5d79f02aae7c
dipghoshraj <dipghoshraj@gmail.com>
2020-11-15 10:14:41 +0530

    pime file

---
+++ c/pimenumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isPrime(int n);
+
+void main()
+{
+    int n = 3;
+    printf("hello world !!");
+
+    bool pime = isPrime(n);
+    printf("%d \n", pime);
+}
+
+bool isPrime(int n)
+{
+    return true;
+}
