b89264a00faea5a591aa3f4afc7bf7ae25908c51
dipghoshraj <dip@spotkwik.com>
2020-11-22 22:43:50 +0530

    Added the encryption function

--- c/encryption.c
+++ c/encryption.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char *encrypt(const char *);
 
 void main()
 {
-    char str[100];
+    char *str;
 
     printf("hello world !!!\n");
     printf("enter the string \n");
-    fgets(str, 100, stdin);
 
-    printf("the string is %s \n", str);
+    str = (char *)malloc(50 * sizeof(char));
+    scanf("%s", str);
+
+    printf("%s\n", str);
+}
+
+const char *encrypt(char str[100])
+{
+    return "encrypt";
+}
+
+const char *decrypt(char str[100])
+{
+    return "decrypt";
 }
