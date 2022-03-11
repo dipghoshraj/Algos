c027631d2f51bc4c3d051a41eb5c2ed64f9dcc5f
Dip Ghosh <dipghosraj@gmail.com>
2022-03-11 18:17:53 +0530

    remove SNN deep learning algo from this repo

--- deep_learning_algo_version_1/SNN.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef _SNN_H
-#define _SNN_H
-double SISO(double input, double weight);
-void SIMO(double* weight, double input, double* output_vector, int LABLES_COUNT);
-#endif // !_SISO_H
-
--- deep_learning_algo_version_1/test_SNN.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-#ifndef _TEST_SNN_H
-#define _TEST_SNN_H
-double test_SISO();
-#endif // !_TEST_SNN_H
